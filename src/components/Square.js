9b658698dec5ee20c3c64a648fbee43f6700d370
Arkaprabha <arkaprabha1234567@gmail.com>
2021-07-01 21:47:32 +0530

    Handeling States and event as- Board of the tictactoe

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-const Square = ({value}) => {
-  return <button type = "button" className = "square">
+const Square = ({value,onClick}) => {
+  return <button 
+  type = "button" 
+  className = "square" 
+  onClick={onClick}
+  >
     {value}
     </button>;
 }
